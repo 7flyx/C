fdd0a2a7219f4aec362fc8b3402151db94bcedb7
7flyx <2647282717@qq.com>
2021-01-10 22:45:45 +0800

    简易通讯录，增删查改操作

---
+++ contact1/contact/contact.c
@@ -0,0 +1,144 @@
+# define _CRT_SECURE_NO_WARNINGS 
+
+#include "contact.h"
+
+
+void Initcontact(struct Con* ps)
+{
+	//将data的数据初始化为0
+	memset(ps->data, 0, sizeof(ps->data));
+	ps->size = 0;
+}
+
+void add_contact(struct Con* ps)
+{
+	//首先判断通讯录是否满了
+	if (ps->size == MAX)
+	{
+		printf("通讯录已满\n");
+	}
+	else
+	{
+		printf("请输入姓名:>");
+		scanf("%s", ps->data[ps->size].name);		
+		printf("请输入年龄:>");
+		scanf("%d", &(ps->data[ps->size].age));
+		printf("请输入性别:>");
+		scanf("%s", ps->data[ps->size].sex);
+		printf("请输入电话:>");
+		scanf("%s", ps->data[ps->size].tele);
+		printf("添加成功\n");
+		ps->size++; //添加完后，size增加，为下次添加做好准备
+	}
+}
+
+void show_contact(const struct Con* ps)
+{
+	//首先判断通讯录是否为空
+	if (ps->size == 0)
+	{
+		printf("通讯录为空\n");
+	}
+	else
+	{
+		int i = 0;
+		printf("%-10s\t%-4s\t%-5s\t%-12s\n", "姓名", "年龄", "性别", "电话");
+		for (i = 0; i < ps->size; i++)
+			printf("%-10s\t%-4d\t%-5s\t%-12s\n",
+				ps->data[i].name,
+				ps->data[i].age,
+				ps->data[i].sex,
+				ps->data[i].tele);
+	}
+}
+
+//查找函数--static修饰，即只能在这个源文件使用
+static int FindByCon(const struct Con* ps, char name[MAX_NAME])
+{
+	int i = 0;
+	for (i = 0; i < ps->size; i++)
+	{
+		if (strcmp(ps->data[i].name, name) == 0)
+		{
+			return i; //找到了，返回该元素下标
+		}
+	}
+	return -1; //没找到，返回-1
+}
+void del_contact(struct Con* ps) //删除信息
+{
+	//查找需要删除信息的那个人
+	char name[MAX_NAME];
+	printf("请输入需要删除人的姓名:>");
+	scanf("%s", name);
+	int pos = FindByCon(ps, name);
+
+	//判断并删除
+	if (-1 == pos)
+	{
+		//是循环完了的，并没有找到要删除的那个人
+		printf("没有找到需要删除的个人信息\n");
+	}
+	else
+	{
+		int j = 0;
+		for (j = pos; j < ps->size - 1; j++)
+		{
+			ps->data[j] = ps->data[j + 1];
+		}
+		printf("删除成功\n");
+		//实则并没删除，只是将后面的数据向前移动了而已，把需要删除的信息覆盖了
+		//ps->size  之所以需要减一，是因为我们在打印的时候，不打印最后那一个元素的信息
+		ps->size--;
+	}
+}
+
+
+void sear_contact(const struct Con* ps)
+{
+	char name[MAX_NAME];
+	printf("请输入需要查找人的信息:>");
+	scanf("%s", name);
+	int pos = FindByCon(ps, name);
+	if (-1 == pos)
+	{
+		//没找到
+		printf("没有找到该信息\n");
+	}
+	else
+	{
+		printf("%-10s\t%-4s\t%-5s\t%-12s\n", "姓名", "年龄", "性别", "电话");
+			printf("%-10s\t%-4d\t%-5s\t%-12s\n",
+				ps->data[pos].name,
+				ps->data[pos].age,
+				ps->data[pos].sex,
+				ps->data[pos].tele);
+	}
+}
+
+void modi_contact(struct Con* ps)
+{
+	//首先输入需要修改对象的姓名
+	char name[MAX_NAME];
+	printf("请输入需要修改人的姓名:>");
+	scanf("%s", name);
+	int pos = FindByCon(ps, name);
+	if (-1 == pos)
+	{
+		//没找到'
+		printf("没有找到需要修改对象的信息\n");
+	}
+	else
+	{
+		printf("请输入姓名:>");
+		scanf("%s", ps->data[pos].name);
+		printf("请输入年龄:>");
+		scanf("%d", &(ps->data[pos].age));
+		printf("请输入性别:>");
+		scanf("%s", ps->data[pos].sex);
+		printf("请输入电话:>");
+		scanf("%s", ps->data[pos].tele);
+
+		printf("修改成功\n");
+	}
+}
---
+++ contact1/contact/test.c
@@ -0,0 +1,53 @@
+# define _CRT_SECURE_NO_WARNINGS 
+#include "contact.h"
+
+
+void menu()
+{
+	printf("*******************************\n");
+	printf("*****  1.add      2.del   *****\n");
+	printf("*****  3.search   4.modifi*****\n");
+	printf("*****  5.show     6.sort  *****\n");
+	printf("*****  0.exit             *****\n");
+	printf("*******************************\n");
+}
+int main()
+{
+	//创建通讯录
+	struct Con con;
+	//初始化通讯录
+	Initcontact(&con);
+	int input = 0;
+	do
+	{
+		menu();
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			add_contact(&con);
+			break;
+		case DEL:
+			del_contact(&con);
+			break;
+		case SEARCH:
+			sear_contact(&con);
+			break;
+		case MODIFI:
+			modi_contact(&con);
+			break;
+		case SHOW:
+			show_contact(&con);
+			break;
+		case SORT:
+			break;
+		case EXIT:
+			printf("退出通讯录\n");
+			break;
+		default :
+			printf("选择错误\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
