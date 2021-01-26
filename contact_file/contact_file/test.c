1cbf7b7f3ab9a85c95a2895f5a0018a1c4f6cf78
7flyx <2647282717@qq.com>
2021-01-26 19:36:41 +0800

    动态、文件版通讯录

---
+++ contact_file/contact_file/test.c
@@ -0,0 +1,60 @@
+# define _CRT_SECURE_NO_WARNINGS 
+#include "contact.h"
+
+
+void menu()
+{
+	printf("*******************************\n");
+	printf("*****  1.add      2.del   *****\n");
+	printf("*****  3.search   4.modifi*****\n");
+	printf("*****  5.show     6.sort  *****\n");
+	printf("*****  7. save    0.exit  *****\n");
+	printf("*******************************\n");
+}
+int main()
+{
+	//创建通讯录
+	struct Con con;
+	//初始化通讯录
+	Initcontact(&con);
+	int input = 0;
+	do
+	{
+		menu();
+		//printf("请选择功能:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			add_contact(&con);
+			break;
+		case DEL:
+			del_contact(&con);
+			break;
+		case SEARCH:
+			sear_contact(&con);
+			break;
+		case MODIFI:
+			modi_contact(&con);
+			break;
+		case SHOW:
+			show_contact(&con);
+			break;
+		case SORT:
+			break;
+		case EXIT:
+			//释放malloc开辟的内存空间
+			Save_contact(&con);
+			DestroyCapacity(&con);
+			printf("退出通讯录\n");
+			break;
+		case SAVE:
+			Save_contact(&con);
+			break;
+		default :
+			printf("选择错误\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
